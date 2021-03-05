f77f23cc05ca1a17e3a9af4bd2bb5e145eedebe6
thanghoangngoc <thanghn@ominext.com>
2021-03-05 15:20:45 +0700

    cm

--- android/app/src/main/java/com/demonative/CustomView.java
+++ android/app/src/main/java/com/demonative/CustomView.java
@@ -1,11 +1,18 @@
 package com.demonative;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CustomView  extends LinearLayout {
+
+public class CustomView extends LinearLayout {
 
     private Context context;
+    private TextView txt;
+    private Button btn;
 
     public CustomView(Context context) {
         super(context);//ADD THIS
@@ -15,6 +22,14 @@ public class CustomView  extends LinearLayout {
 
     public void init() {
         inflate(context, R.layout.test, this);
+        txt = findViewById(R.id.txt1);
+        btn = findViewById(R.id.btn1);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
